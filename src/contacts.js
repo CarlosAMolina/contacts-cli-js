a66fe0ca7a7e71db3a21436849152076543f7f20
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-09 21:45:06 +0200

    style(contacts): add indentation

--- src/contacts.js
+++ src/contacts.js
@@ -3,20 +3,20 @@ import { request } from 'graphql-request'
 export const findContacts = async (filter) => {
     console.log(`Searching ${filter}`);
     const query = `
-    query findContacts($filter: String) {
-      contacts(filter:$filter) {
-        categories
-        id
-        name
-        nicknames
-        phones {
-          description,
-          number
+      query findContacts($filter: String) {
+        contacts(filter:$filter) {
+          categories
+          id
+          name
+          nicknames
+          phones {
+            description,
+            number
+          }
+          surname
         }
-        surname
       }
-    }
-  `
+    `
     const variables = { filter: filter }
     const { contacts } = await request('http://localhost:4000/graphql', query, variables);
     return contacts;
