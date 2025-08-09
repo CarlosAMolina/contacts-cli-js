76e753f0ebd94db47591825ad4228547bfa6ea26
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-09 22:16:30 +0200

    feat(contacts): add get summary

--- src/contacts.js
+++ src/contacts.js
@@ -17,12 +17,21 @@ export const findContacts = async (filter) => {
         }
       }
     `
-    const variables = { filter: filter }
+    const variables = { filter: filter };
     const { contacts } = await request('http://localhost:4000/graphql', query, variables);
-    return contacts;
+    const summary_array = contacts.map(contact => getContactSummary(contact));
+    return summary_array.join('\n');
 }
 
 export const getIdContact = async (id) => {
     console.log(`Getting ID ${id} is not implemented`);
     return 1;
+}
+
+const getContactSummary = (contact) => {
+    const categories = contact.categories.join(', ');
+    const result_array = contact.phones.map(phone => 
+        `${phone.number} (${phone.description}) ${contact.name} ${contact.surname}. ${categories}. ID ${contact.id}`
+    );
+    return result_array.join('\n');
 }
