36da9cb882bc5217e6929d10799149041208ea34
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-09 20:35:17 +0200

    feat(command): add search commands

---
+++ src/command.js
@@ -0,0 +1,25 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { findContacts, getIdContact } from './contacts.js'
+
+yargs(hideBin(process.argv))
+  .command('contacts <filter>', 'get matching contacts', yargs => {
+    return yargs.positional('filter', {
+      describe: 'The search term to filter contacts by',
+      type: 'string'
+    })
+  }, async (argv) => {
+      const matches = await findContacts(argv.filter)
+      console.log(matches)
+  })
+  .command('id <id>', 'get a contact by id', yargs => {
+    return yargs.positional('id', {
+      describe: 'The id of the contact to return',
+      type: 'number'
+    })
+  }, async (argv) => {
+      const contact = await getIdContact(argv.id)
+      console.log(contact)
+  })
+  .demandCommand(1)
+  .parse()
---
+++ src/contacts.js
@@ -0,0 +1,9 @@
+export const findContacts = async (filter) => {
+  console.log(`Searching ${filter}`);
+  return 1;
+}
+
+export const getIdContact = async (id) => {
+  console.log(`Getting ID ${id}`);
+  return 1;
+}
