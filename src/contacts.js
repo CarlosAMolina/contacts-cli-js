e9d6db97b640b3eab4c0f9e91e67c72b67598e8c
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-09 21:20:12 +0200

    feat(contacts): add graphql request

--- src/contacts.js
+++ src/contacts.js
@@ -1,5 +1,25 @@
+import { request } from 'graphql-request'
+
 export const findContacts = async (filter) => {
   console.log(`Searching ${filter}`);
+  const query = `
+    {
+      contacts {
+        categories
+        id
+        name
+        nicknames
+        phones {
+          description,
+          number
+        }
+        surname
+      }
+    }
+  `
+  request('http://localhost:4000/graphql', query)
+    .then(console.log)
+    .catch(console.error)
   return 1;
 }
 
